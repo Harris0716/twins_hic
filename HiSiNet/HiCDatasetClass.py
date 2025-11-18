611e0e6de8f09284a8b65ae5d7131f06c1f0f42e
Guan <112753134@g.nccu.edu.tw>
2025-11-18 19:50:18 +0800

    debug print to count  no of siamese data

--- HiSiNet/HiCDatasetClass.py
+++ HiSiNet/HiCDatasetClass.py
@@ -148,9 +148,36 @@ class SiameseHiCDataset(HiCDataset):
         pass
 
     def append_data(self, curr_data, pos):
-        self.data.extend([(curr_data[k][0], curr_data[j][0], (self.sims[0] if curr_data[k][1] == curr_data[j][1] else self.sims[1]) ) for k in range(0,len(curr_data)) for j in range(k+1,len(curr_data))])
-        self.positions.extend( [pos for k in range(0,len(curr_data)) for j in range(k+1,len(curr_data))])
-        self.labels.extend( [( k, j) for k in range(0,len(curr_data)) for j in range(k+1,len(curr_data))])
+        # debug print 
+        print(f"\n=== Position {pos} ===")
+        print("curr_data content:")
+
+        # curr_data[k] = (image_tensor, class_id)
+        for idx, item in enumerate(curr_data):
+            img, class_id = item
+            print(f"  idx={idx}, class_id={class_id}")
+
+        pair_count = 0
+
+        for k in range(len(curr_data)):
+            for j in range(k + 1, len(curr_data)):
+
+                img1, class1 = curr_data[k]
+                img2, class2 = curr_data[j]
+
+                label = self.sims[0] if class1 == class2 else self.sims[1]
+                pair_type = "REP" if class1 == class2 else "COND"
+
+                print(f"Pair {pair_count+1}: (class({class1}), class({class2}), {pair_type})")
+
+                # 實際加入 dataset
+                self.data.append((img1, img2, label))
+                self.positions.append(pos)
+                self.labels.append((k, j))
+
+                pair_count += 1
+
+        print(f"Total pairs added at this pos: {pair_count}")
 
     def make_data(self, list_of_HiCDatasets):
         datasets = len(list_of_HiCDatasets)
